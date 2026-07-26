85e1c7151db651d17b5499772e4d61416b4f2800
User1012007 <rojasbadilloe@gmail.com>
2026-07-26 13:43:02 -0600

    flake.nix: nix development shell

--- flake.nix
+++ flake.nix
@@ -31,8 +31,27 @@
       let
         pkgs = (import nixpkgs) {
           inherit system overlays;
+
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
         };
+
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          cmdLineToolsVersion = "13.0";
+          toolsVersion = "26.1.1";
+          platformVersions = [
+            "34"
+            "33"
+          ];
+          buildToolsVersions = [ "34.0.0" ];
+          includeEmulator = true;
+        };
+
+        androidSdk = androidComposition.androidsdk;
       in
+
       rec {
         packages.arduino-cli = pkgs.wrapArduinoCLI {
           libraries = with pkgs.arduinoLibraries; [
@@ -48,16 +67,29 @@
             platforms.rp2040.rp2040."2.3.3"
           ];
         };
-        devshells.default = pkgs.mkShell {
+
+        devShells.default = pkgs.mkShell {
           buildInputs = [
             self.packages.${system}.arduino-cli
             pkgs.picocom
             pkgs.clang-tools
+            pkgs.arduino-ide
 
-            flutter
-            android-sdk
-            android-studio
+            pkgs.flutter
+            pkgs.androidsdk
+            pkgs.android-studio
           ];
+          shellHook = ''
+            export ANDROID_HOME="${androidSdk}/libexec/android-sdk"
+            export ANDROID_SDK_ROOT="${androidSdk}/libexec/android-sdk"
+            export JAVA_HOME="${pkgs.jdk17}"
+            export CHROME_EXECUTABLE="${pkgs.chromium}/bin/chromium"
+
+            export NIXPKGS_ACCEPT_ANDROID_SDK_LICENSE=1
+            export NIXPKGS_ALLOW_UNFREE=1
+            export PKG_CONFIG_PATH="${pkgs.gtk3.dev}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig:${pkgs.pango.dev}/lib/pkgconfig:${pkgs.cairo.dev}/lib/pkgconfig:${pkgs.gdk-pixbuf.dev}/lib/pkgconfig:${pkgs.atk.dev}/lib/pkgconfig"
+
+          '';
         };
       }
     ));
